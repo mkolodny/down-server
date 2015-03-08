b7aa847a7f68545c2e6d1babb9d74077f3b6359e
Michael Kolodny <michaelckolodny@gmail.com>
2015-03-07 19:53:45 -0500

    set postgis version in all environments

--- down/settings/prod.py
+++ down/settings/prod.py
@@ -3,3 +3,6 @@ from .common import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+# DB
+POSTGIS_VERSION = (2, 1, 3)
--- down/settings/staging.py
+++ down/settings/staging.py
@@ -3,3 +3,6 @@ from .common import *
 
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+# DB
+POSTGIS_VERSION = (2, 1, 3)
