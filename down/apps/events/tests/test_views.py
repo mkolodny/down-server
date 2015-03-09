40fc175050c167d5026fbe2fa6d4d131cc07f140
Michael Kolodny <michaelckolodny@gmail.com>
2015-03-09 15:24:57 -0400

    return a 201 response when creating event message notifications

--- down/apps/events/tests/test_views.py
+++ down/apps/events/tests/test_views.py
@@ -17,6 +17,17 @@ class EventTests(APITestCase):
         self.user = User(email='aturing@gmail.com', name='Alan Tdog Turing')
         self.user.save()
 
+        # Mock a place.
+        self.place = Place(geo='POINT(40.6898319 -73.9904645)')
+        self.place.save()
+
+        # Mock an event.
+        self.event = Event(title='bars?!?!!', creator=self.user,
+                      datetime=timezone.now(), place=self.place,
+                      description='bars!!!!')
+        self.event.save()
+
+
     def test_create(self):
         url = reverse('event-list')
         data = {
@@ -46,24 +57,19 @@ class EventTests(APITestCase):
         self.assertEqual(response.content, json_event)
 
     def test_get(self):
-        # Mock a place.
-        place = Place(geo='POINT(40.6898319 -73.9904645)')
-        place.save()
-
-        # Mock an event.
-        event = Event(title='bars?!?!!', creator=self.user,
-                      datetime=timezone.now(), place=place,
-                      description='bars!!!!')
-        event.save()
-
-        url = reverse('event-detail', kwargs={'pk': event.id})
+        url = reverse('event-detail', kwargs={'pk': self.event.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # It should return the event.
-        serializer = EventSerializer(event)
+        serializer = EventSerializer(self.event)
         json_event = JSONRenderer().render(serializer.data)
         self.assertEqual(response.content, json_event)
+
+    def test_create_message(self):
+        url = reverse('event-messages', kwargs={'pk': self.event.id})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 class InvitationTests(APITestCase):
--- down/apps/events/views.py
+++ down/apps/events/views.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 from .models import Event, Invitation
 from .serializers import EventSerializer, InvitationSerializer
 
@@ -8,6 +10,13 @@ from .serializers import EventSerializer, InvitationSerializer
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
+
+    @detail_route(methods=['post'])
+    def messages(self, request, pk=None):
+        # Notify the user's who are down for the event (except for the user that
+        # posted the message) about a new message.
+        return Response(status=status.HTTP_201_CREATED)
+
 
 
 # TODO: Security
