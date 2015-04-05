7f2db076a0c471d5a2edcbc2144ddc6453b901a4
Viraj Sinha <virajosinha@gmail.com>
2015-04-05 16:36:29 -0400

    sends push notifications when a user adds someone as a friend, and when the user is added back

--- down/apps/friends/models.py
+++ down/apps/friends/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 from django.contrib.gis.db import models
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from push_notifications.models import APNSDevice
 from down.apps.auth.models import User
 
 
@@ -14,3 +17,24 @@ class Friendship(models.Model):
 
     class Meta:
         unique_together = ('user', 'friend')
+
+@receiver(post_save, sender=Friendship)
+def send_new_friendship_notification(sender, instance, created, **kwargs):
+    """
+    Send a push notification to the user who was added as a friend
+    """
+
+    if not created:
+        return
+
+    friendship = instance
+
+    # check if one leg of this friendship already exists in the database
+    if Friendship.objects.filter(user=friendship.friend, friend=friendship.user):
+        message = '{name} added you back!'.format(name=friendship.user.name)
+    else:
+        message = '{name} added you as a friend!'.format(name=friendship.user.name)
+
+
+    devices = APNSDevice.objects.filter(user_id=friendship.friend.id)
+    devices.send_message(message)
--- down/apps/friends/tests/test_models.py
+++ down/apps/friends/tests/test_models.py
@@ -1,4 +1,65 @@
 from __future__ import unicode_literals
-from django.test import TestCase
+import mock
+from push_notifications.models import APNSDevice
+from rest_framework.test import APITestCase
 from down.apps.auth.models import User
-from ..models import Friendship
+from down.apps.friends.models import Friendship
+
+class FriendTests(APITestCase):
+
+    def setUp(self):
+        # Mock a user
+        self.user = User(name="Alan Tdog Turing")
+        self.user.save()
+
+        # Mock the user's device.
+        registration_id1 = ('2ed202ac08ea9033665e853a3dc8bc4c5e78f7a6cf8d559'
+                           '10df230567037dcc4')
+        device_id1 = 'E621E1F8-C36C-495A-93FC-0C247A3E6E5F'
+        self.user_apns_device = APNSDevice(registration_id=registration_id1,
+                                      device_id=device_id1, name='iPhone, 8.2',
+                                      user=self.user)
+        self.user_apns_device.save()
+
+        # Mock another user to be added as a friend by the user
+        self.friend = User(name="Richard FoShizzle Feynman")
+        self.friend.save()
+
+        # Mock the friend's device.
+        registration_id2 = ('3ed202ac08ea9033665e853a3dc8bc4c5e78f7a6cf8d559'
+                           '20df230567037dcc4')
+        device_id2 = 'E622E2F8-C36C-495A-93FC-0C247A3E6E5F'
+        self.friend_apns_device = APNSDevice(registration_id=registration_id2,
+                                      device_id=device_id2, name='iPhone, 8.2',
+                                      user=self.friend)
+        self.friend_apns_device.save()
+
+    @mock.patch('push_notifications.apns.apns_send_bulk_message')
+    def test_friendship_create_notify(self, mock_send): 
+
+        # Add one direction of the friendship
+        friendship = Friendship(user=self.user, friend=self.friend)
+        friendship.save()
+
+        # It should notify the friend that they were added by the user
+        token = self.friend_apns_device.registration_id
+        message = '{name} added you as a friend!'.format(name=self.user.name)
+        mock_send.assert_called_once_with(registration_ids=[token], alert=message)
+
+    @mock.patch('push_notifications.apns.apns_send_bulk_message')
+    def test_friendship_create_notify_added_back(self, mock_send): 
+
+        # Add one direction of the friendship
+        friendship = Friendship(user=self.user, friend=self.friend)
+        friendship.save()
+
+        # Add the other direction of the friendship
+        friendship = Friendship(user=self.friend, friend=self.user)
+        friendship.save()
+
+        # It should notify the user that they were added by the friend 
+        # but with a different message!!!
+        token = self.user_apns_device.registration_id
+        message = '{name} added you back!'.format(name=self.friend.name)
+        mock_send.assert_called_with(registration_ids=[token], alert=message)
+
