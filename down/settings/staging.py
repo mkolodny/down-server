b1869b0564faf43c03ef826ec630649dee29093c
Michael Kolodny <michaelckolodny@gmail.com>
2015-04-09 14:46:24 -0400

    use the apns sandbox domain in staging

--- down/settings/staging.py
+++ down/settings/staging.py
@@ -15,4 +15,6 @@ GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 # Push notifications
 PUSH_NOTIFICATIONS_SETTINGS = {
     'APNS_CERTIFICATE': os.path.join(BASE_DIR, 'config/apns/dev/certkey.pem'),
+    'APNS_HOST': 'gateway.sandbox.push.apple.com',
+    'APNS_FEEDBACK_HOST': 'feedback.sandbox.push.apple.com',
 }
