d2d4e2eba942bc0d6f709ee822420cb257807fa1
Michael Kolodny <michaelckolodny@gmail.com>
2015-08-28 13:47:38 -0400

    redirect to the app store from the landing page

--- down/apps/auth/tests/test_views.py
+++ down/apps/auth/tests/test_views.py
@@ -868,12 +868,21 @@ class TermsTests(APITestCase):
 
 class LandingTests(APITestCase):
 
+    """
     def test_get(self):
         url = reverse('landing')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTemplateUsed(response, 'landing.html')
+    """
 
+    def test_redirect(self):
+        url = reverse('landing')
+        response = self.client.get(url)
+        app_store_url = ('https://itunes.apple.com/us/app/down-connect-people'
+                         '-around/id969040287?mt=8')
+        self.assertRedirects(response, app_store_url,
+                             fetch_redirect_response=False)
 
 class LinfootFunnelTests(APITestCase):
 
