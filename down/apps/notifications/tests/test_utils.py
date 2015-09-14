42fc874515e76ccbeb3023c4e2b91ce023dc5db9
Michael Kolodny <michaelckolodny@gmail.com>
2015-09-14 15:15:23 -0400

    handle when a user has an ios device and an android device

--- down/apps/notifications/tests/test_utils.py
+++ down/apps/notifications/tests/test_utils.py
@@ -12,34 +12,31 @@ class SendMessageTests(TestCase):
     def setUp(self):
         # Mock three users: 1) an iphone user; 2) an android user; 3) a user added
         # from contacts.
-        self.ios_user = User(email='aturing@gmail.com', name='Alan Tdog Turing',
-                             username='tdog')
-        self.ios_user.save()
+        self.user = User(email='aturing@gmail.com', name='Alan Tdog Turing',
+                         username='tdog')
+        self.user.save()
         registration_id = ('1ed202ac08ea9033665e853a3dc8bc4c5e78f7a6cf8d559'
                            '10df230567037dcc4')
         device_id = 'E621E1F8-C36C-495A-93FC-0C247A3E6E5F'
-        self.ios_user_device = APNSDevice(registration_id=registration_id,
-                                          device_id=device_id, name='iPhone, 8.2',
-                                          user=self.ios_user)
-        self.ios_user_device.save()
+        self.ios_device = APNSDevice(registration_id=registration_id,
+                                     device_id=device_id, name='iPhone, 8.2',
+                                     user=self.user)
+        self.ios_device.save()
 
-        self.android_user = User(email='jclarke@gmail.com', name='Joan Clarke',
-                                 username='jcke')
-        self.android_user.save()
         registration_id = ('APA91bHPRgkF3JUikC4ENAHEeMrd41Zxv3hVZjC9KtT8OvP'
                            'VGJ-hQMRKRrZuJAEcl7B338qju59zJMjw2DELjzEvxwYv7h'
                            'H5Ynpc1ODQ0aT4U4OFEeco8ohsN5PjL1iC2dNtk2BAokeMC'
                            'g2ZXKqpc8FXKmhX94kIxQaa')
         device_id = int('490154203237518', 16)
-        self.android_user_device = GCMDevice(registration_id=registration_id,
-                                             device_id=device_id, name='Galaxy S6',
-                                             user=self.android_user)
-        self.android_user_device.save()
+        self.android_device = GCMDevice(registration_id=registration_id,
+                                        device_id=device_id, name='Galaxy S6',
+                                        user=self.user)
+        self.android_device.save()
 
-        self.sms_user = User(name='Bruce Lee') # SMS users don't have a username.
-        self.sms_user.save()
-        self.sms_user_phone = UserPhone(user=self.sms_user, phone='+12036227310')
-        self.sms_user_phone.save()
+        self.contact = User(name='Bruce Lee') # SMS users don't have a username.
+        self.contact.save()
+        self.contact_phone = UserPhone(user=self.contact, phone='+12036227310')
+        self.contact_phone.save()
 
     @mock.patch('push_notifications.apns.apns_send_bulk_message')
     @mock.patch('push_notifications.gcm.gcm_send_bulk_message')
@@ -49,17 +46,17 @@ class SendMessageTests(TestCase):
         mock_client = mock.MagicMock()
         mock_twilio.return_value = mock_client
 
-        user_ids = [self.ios_user.id, self.android_user.id, self.sms_user.id]
+        user_ids = [self.user.id, self.contact.id]
         message = 'Bars?!?!?!'
         utils.send_message(user_ids, message)
 
         # It should send push notifications to users with ios devices.
-        token = self.ios_user_device.registration_id
+        token = self.ios_device.registration_id
         mock_apns.assert_any_call(registration_ids=[token], alert=message,
                                   badge=1)
 
         # It should send push notifications to users with android devices.
-        token = self.android_user_device.registration_id
+        token = self.android_device.registration_id
         data = {'message': message}
         mock_gcm.assert_any_call(registration_ids=[token], data=data)
 
@@ -68,7 +65,7 @@ class SendMessageTests(TestCase):
                                        settings.TWILIO_TOKEN)
 
         # It should send SMS to users without devices.
-        phone = unicode(self.sms_user_phone.phone)
+        phone = unicode(self.contact_phone.phone)
         mock_client.messages.create.assert_called_with(to=phone, 
                                                        from_=settings.TWILIO_PHONE,
                                                        body=message)
@@ -76,17 +73,17 @@ class SendMessageTests(TestCase):
     @mock.patch('push_notifications.apns.apns_send_bulk_message')
     @mock.patch('push_notifications.gcm.gcm_send_bulk_message')
     def test_send_message_devices_only(self, mock_gcm, mock_apns):
-        user_ids = [self.ios_user.id, self.android_user.id, self.sms_user.id]
+        user_ids = [self.user.id, self.contact.id]
         message = 'Bars?!?!?!'
         utils.send_message(user_ids, message, sms=False)
 
         # It should send push notifications to users with ios devices.
-        token = self.ios_user_device.registration_id
+        token = self.ios_device.registration_id
         mock_apns.assert_any_call(registration_ids=[token], alert=message,
                                   badge=1)
 
         # It should send push notifications to users with android devices.
-        token = self.android_user_device.registration_id
+        token = self.android_device.registration_id
         data = {'message': message}
         mock_gcm.assert_any_call(registration_ids=[token], data=data)
 
@@ -98,23 +95,23 @@ class SendMessageTests(TestCase):
         mock_client = mock.MagicMock()
         mock_twilio.return_value = mock_client
 
-        user_ids = [self.ios_user.id, self.android_user.id, self.sms_user.id]
+        user_ids = [self.user.id, self.contact.id]
         message = 'from Barack Obama'
         utils.send_message(user_ids, message, is_invitation=True)
 
         # It should send push notifications to users with ios devices.
-        token = self.ios_user_device.registration_id
+        token = self.ios_device.registration_id
         mock_apns.assert_any_call(registration_ids=[token], alert=message,
                                   badge=1)
 
         # It should send push notifications to users with android devices.
-        token = self.android_user_device.registration_id
+        token = self.android_device.registration_id
         data = {'message': message}
         mock_gcm.assert_any_call(registration_ids=[token], data=data)
 
         # It should send SMS to users without devices.
         message = 'Down. {og_message}'.format(og_message=message)
-        phone = unicode(self.sms_user_phone.phone)
+        phone = unicode(self.contact_phone.phone)
         mock_client.messages.create.assert_called_with(to=phone, 
                                                        from_=settings.TWILIO_PHONE,
                                                        body=message)
--- down/apps/notifications/utils.py
+++ down/apps/notifications/utils.py
@@ -10,21 +10,16 @@ def send_message(user_ids, message, sms=True, is_invitation=False):
     apnsdevices.send_message(message, badge=1)
 
     # Notify users with Android devices.
-    apnsdevice_ids = set(device.user_id for device in apnsdevices)
-    remaining_user_ids = [user_id for user_id in user_ids
-            if user_id not in apnsdevice_ids]
-    gcmdevices = GCMDevice.objects.filter(user_id__in=remaining_user_ids)
+    gcmdevices = GCMDevice.objects.filter(user_id__in=user_ids)
     gcmdevices.send_message(message, badge=1)
 
     if sms:
         # Notify users who were added from contacts.
-        gcmdevice_ids = set(device.user_id for device in gcmdevices)
-        remaining_user_ids = [user_id for user_id in remaining_user_ids
-                if user_id not in gcmdevice_ids]
-        userphones = UserPhone.objects.filter(user_id__in=remaining_user_ids)
-        client = TwilioRestClient(settings.TWILIO_ACCOUNT, settings.TWILIO_TOKEN)
         if is_invitation:
             message = 'Down. {og_message}'.format(og_message=message)
+        client = TwilioRestClient(settings.TWILIO_ACCOUNT, settings.TWILIO_TOKEN)
+        userphones = UserPhone.objects.filter(user_id__in=user_ids,
+                                              user__username__isnull=True)
         for userphone in userphones:
             phone = unicode(userphone.phone)
             client.messages.create(to=phone, from_=settings.TWILIO_PHONE,
