13205859ee35dd1611346cc8181335b101bc1ab1
Michael Kolodny <michaelckolodny@gmail.com>
2015-10-21 14:25:07 -0400

    bulk create the chats

--- rallytap/apps/friends/migrations/0017_auto_20151021_1810.py
+++ rallytap/apps/friends/migrations/0017_auto_20151021_1810.py
@@ -6,25 +6,26 @@ from django.conf import settings
 from rallytap.apps.utils.utils import add_members
 
 
-def teamrallytap(apps, schema_editor):
+def befriend_teamrallytap(apps, schema_editor):
     if settings.ENV == 'dev':
         return
 
     User = apps.get_model('down_auth', 'User')
     Friendship = apps.get_model('friends', 'Friendship')
     teamrallytap = User.objects.get(username='teamrallytap')
+    friendships = []
     for user in User.objects.all().exclude(id=teamrallytap.id):
-        # Create a chat on the Meteor server.
-        chat_id = '{user_id},{friend_id}'.format(user_id=user.id,
-                                                 friend_id=teamrallytap.id)
-        user_ids = [user.id, teamrallytap.id]
-        add_members(chat_id, user_ids)
+        # Make the user friends with Team Rallytap.
+        friendship.append(Friendship(user=user, friend=teamrallytap))
+        friendship.append(Friendship(user=teamrallytap, friend=user))
+    friendships.bulk_create(friendships)
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('friends', '0017_auto_20151021_0342'),
+        ('friends', '0016_remove_friendship_was_acknowledged'),
     ]
 
     operations = [
+        migrations.RunPython(befriend_teamrallytap),
     ]
---
+++ rallytap/apps/friends/migrations/0018_auto_20151021_1819.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import requests
+from rallytap.apps.utils.utils import add_members
+
+
+def create_teamrallytap_chats(apps, schema_editor):
+    if settings.ENV == 'dev':
+        return
+
+    User = apps.get_model('down_auth', 'User')
+    teamrallytap = User.objects.get(username='teamrallytap')
+    chats = []
+    for user in User.objects.all().exclude(id=teamrallytap.id):
+        chats.append({'chat_id': chat_id, 'user_ids': [user.id, teamrallytap.id]})
+
+    # Create the chats.
+    url = '{meteor_url}/chats/members'.format(meteor_url=settings.METEOR_URL)
+    data = json.dumps({'chats': chats})
+    auth_header = 'Token {api_key}'.format(api_key=settings.METEOR_KEY)
+    headers = {
+        'Authorization': auth_header,
+        'Content-Type': 'application/json',
+    }
+    response = requests.post(url, data=data, headers=headers)
+    response.raise_for_status()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('friends', '0017_auto_20151021_1810'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_teamrallytap_chats),
+    ]
