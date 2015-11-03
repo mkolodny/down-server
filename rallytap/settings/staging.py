834ea0629d610a9fc075fa1c0bb3b9a6596e04f7
Michael Kolodny <michaelckolodny@gmail.com>
2015-11-03 17:46:09 -0500

    hide the staging geodjango env vars

--- rallytap/settings/staging.py
+++ rallytap/settings/staging.py
@@ -9,9 +9,11 @@ TEMPLATE_DEBUG = False
 POSTGIS_VERSION = (2, 1, 5)
 
 # GeoDjango
+"""
 GEOS_LIBRARY_PATH = '{}/libgeos_c.so'.format(os.environ.get('GEOS_LIBRARY_PATH'))
 GDAL_LIBRARY_PATH = '{}/libgdal.so'.format(os.environ.get('GDAL_LIBRARY_PATH'))
 PROJ4_LIBRARY_PATH = '{}/libproj.so'.format(os.environ.get('PROJ4_LIBRARY_PATH'))
+"""
 
 # Push notifications
 PUSH_NOTIFICATIONS_SETTINGS.update({
